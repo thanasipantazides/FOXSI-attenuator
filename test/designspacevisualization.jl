b0fc0801d81f7c3f88a8166efa9fdeec3c1d5046
thanasipantazides <thanasipantazides@gmail.com>
2021-08-30 11:53:11 -0500

    cleaner ray visualization

--- test/designspacevisualization.jl
+++ test/designspacevisualization.jl
@@ -4,7 +4,7 @@ using DataFrames
 using CSV
 
 # import design space data:
-designpath = joinpath(@__DIR__, "../results/designspace_1e5csv")
+designpath = joinpath(@__DIR__, "../results/designspace_1e5.csv")
 designspace = CSV.read(designpath, DataFrame)
 E = designspace.energy
 v1 = designspace.v1
@@ -15,3 +15,10 @@ r02 = designspace.r02
 r03 = designspace.r03
 absorbprob = designspace.absorbprob
 
+v = [v1'; v2'; v3']
+r0 = [r01'; r02'; r03']
+slice = NaN*r0
+plotdata = permutedims(cat(cat(r0, v, dims=3), slice, dims=3), (1,3,2))
+
+plotlyjs()
+plot(vec(plotdata[1, :, :]), vec(plotdata[2, :, :]), vec(plotdata[3, :, :]), legend=false, color=:violet)
